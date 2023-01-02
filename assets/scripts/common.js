316c9326456a8e11110f2cde764769829664fa60
LAPTOP-F1JSED2K\abbey <ldh@abbeyroad.asia>
2023-01-02 14:51:48 +0900

    about,contact 작업

--- assets/scripts/common.js
+++ assets/scripts/common.js
@@ -1,5 +1,6 @@
 
-
+let vh = window.innerHeight * 0.01
+document.documentElement.style.setProperty('--vh', `${vh}px`)
 
 function mainLoaded (){
   let iframes = document.querySelectorAll('iframe');
